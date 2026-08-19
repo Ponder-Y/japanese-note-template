d1752c39fdce57c6fa11bdfa3aec509961ba0cae
Ponder-Y <etfcrawler@gmail.com>
2026-08-19 09:57:55 +0800

    Use notebook React shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import NotebookApp from './NotebookApp.jsx';
 import '../styles.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <NotebookApp />
   </StrictMode>
 );
