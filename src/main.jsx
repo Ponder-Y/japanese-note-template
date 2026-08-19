8404a247db5842ecd383918059dd365516d4d307
Ponder-Y <etfcrawler@gmail.com>
2026-08-19 10:33:57 +0800

    Load supplemental N3 grammar notes

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './extra-notes.js';
 import NotebookApp from './NotebookApp.jsx';
 import '../styles.css';
 
